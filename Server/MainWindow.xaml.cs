003e33915fb1dc7009e184976d8771aa62a692bc
ChaozFire <Bryandu0ng@yahoo.com>
2018-03-05 14:55:27 -0800

    Client-Server Interaction

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -18,22 +18,40 @@ using System.Net.Security;
 using System.Net.Sockets;
 using Newtonsoft.Json;
 using Telemeal.Model;
+using System.Threading;
 
 namespace Server
 {
+    public class OrderItem
+    {
+        public int OrderItemID { get; set; }
+        public String FoodName { get; set; }
+        public DateTime TimePurchased { get; set; }
+        public Boolean IsTakeOut { get; set; }
+
+        public OrderItem(int id, string name, DateTime time, Boolean takeout)
+        {
+            OrderItemID = id;
+            FoodName = name;
+            TimePurchased = time;
+            IsTakeOut = takeout;
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<OrderItem> items = new List<OrderItem>();
+
         public MainWindow()
         {
-<<<<<<< HEAD
             InitializeComponent();
+            itemCart.ItemsSource = items;
+            //IsBackground makes sure thread does not keep running after window has been closed
+            new Thread(() => WaitForResponse()) { IsBackground = true }.Start() ;
         }
-=======
-            //InitializeComponent();
->>>>>>> master
 
         public void WaitForResponse()
         {
@@ -53,11 +71,22 @@ namespace Server
 
                 // Read the message, and perform different actions  
                 message = cleanMessage(buffer);
-                //Order order = JsonConvert.DeserializeObject<Order>(message);
+                Order order = JsonConvert.DeserializeObject<Order>(message);
 
-                //Console.WriteLine(order.OrderID);
+                foreach(Food f in order.Foods)
+                {
+                    OrderItem item = new OrderItem(order.OrderID, f.Name, order.OrderDateTime, order.IsTakeOut);
+                    items.Add(item);
+                }
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    itemCart.Items.Refresh();
+                });
             }
         }
+
+
 
         private static string cleanMessage(byte[] bytes)
         {
