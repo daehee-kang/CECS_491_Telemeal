4cec7ba68e9fa22d058b3c3741fdd26ca47eaab9
daehee-kang <daehee6316@gmail.com>
2018-03-05 16:00:24 -0800

    commenting (2)

--- Telemeal/Model/dbConnection.cs
+++ Telemeal/Model/dbConnection.cs
@@ -12,20 +12,31 @@ namespace Telemeal.Model
 {
     class dbConnection
     {
-        private static string ConnectionString;// = @"Data Source=sqliteTMDB.db;Version=3;New=false;Compress=True";
+        private static string ConnectionString;
         public SQLiteConnection sqlite_conn;
         public SQLiteCommand sqlite_cmd;
         public SQLiteDataReader sqlite_dr;
 
+        /// <summary>
+        /// constructor
+        /// </summary>
         public dbConnection()
         {
+            //Connection string
             string v = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"../../"));
             ConnectionString = string.Format($@"Data Source={v}sqliteTMDB.db;Version=3;New=false;Compress=True");
+            //initialize member data
             sqlite_conn = new SQLiteConnection(ConnectionString);
+            //open the connection
             sqlite_conn.Open();
             sqlite_cmd = sqlite_conn.CreateCommand();
         }
 
+        /// <summary>
+        /// This method will create new food table in the database
+        /// id: primary key
+        /// name, price, category: unique key (sub-key)
+        /// </summary>
         public void CreateFoodTable()
         {
             string cmd = $"CREATE TABLE Food " +
@@ -41,6 +52,10 @@ namespace Telemeal.Model
             sqlite_cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// this method will create new employee table in the database
+        /// id, name: primary key
+        /// </summary>
         public void CreateEmployeeTable()
         {
             string cmd = $"CREATE TABLE Employee " +
@@ -53,6 +68,10 @@ namespace Telemeal.Model
             sqlite_cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// this method will insert new food item into the food table
+        /// </summary>
+        /// <param name="food">Food object to be added to the table</param>
         public void InsertFood(Food food)
         {
             string name = food.Name;
@@ -72,6 +91,10 @@ namespace Telemeal.Model
             sqlite_cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// this method will insert new employee object into the employee table
+        /// </summary>
+        /// <param name="employee">Employee object to be added to the table</param>
         public void InsertEmployee(Employee employee)
         {
             int employeeID = employee.ID;
@@ -83,6 +106,10 @@ namespace Telemeal.Model
             sqlite_cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// this method will update the change made in any object of same name
+        /// </summary>
+        /// <param name="food">Food object which has same name but different properties to be updated</param>
         public void UpdateFood(Food food) {
             string name = food.Name;
             double price = food.Price;
@@ -92,8 +119,8 @@ namespace Telemeal.Model
             int subCtr = (int)food.SubCtgr;
             
             string cmd = $"UPDATE Food " +
-                $"SET name = @name, price = @price, desc = @desc, img = @img, mainctgr = @mainCtr, subctgr = @subCtr " +
-                $"WHERE id = @foodID";
+                $"SET price = @price, desc = @desc, img = @img, mainctgr = @mainCtr, subctgr = @subCtr " +
+                $"WHERE name = @@name";
             sqlite_cmd = new SQLiteCommand(cmd, sqlite_conn);
 
             sqlite_cmd.Parameters.AddWithValue("@name", name);
@@ -105,6 +132,11 @@ namespace Telemeal.Model
             sqlite_cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// this method will remove data with matching name and price from the table
+        /// </summary>
+        /// <param name="name">Name of the food to be deleted</param>
+        /// <param name="price">Price of the food to be deleted</param>
         public void DeleteFoodByNameAndPrice(string name, double price)
         {
             string cmd = $"DELETE FROM Food WHERE name = '{name}' AND price = {price}";
@@ -112,6 +144,10 @@ namespace Telemeal.Model
             sqlite_cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// this method will remove data with matching name
+        /// </summary>
+        /// <param name="name">Name of the food to be deleted</param>
         public void DeleteFoodByName(string name)
         {
             string cmd = $"DELETE FROM Food WHERE name = '{name}'";
@@ -119,6 +155,10 @@ namespace Telemeal.Model
             sqlite_cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// this method will remove data with matching name from the table
+        /// </summary>
+        /// <param name="name">Name of employee to be deleted</param>
         public void DeleteEmployeeByName(string name)
         {
             string cmd = $"DELETE FROM Employee WHERE name = '{name}'";
@@ -126,6 +166,10 @@ namespace Telemeal.Model
             sqlite_cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// this method will delete the table with matching name
+        /// </summary>
+        /// <param name="name">Name of the table to be deleted</param>
         public void DeleteTable(string name)
         {
             string cmd = $"DROP TABLE {name}";
@@ -133,6 +177,11 @@ namespace Telemeal.Model
             sqlite_cmd.ExecuteNonQuery();
         }
 
+        /// <summary>
+        /// this method will return SQLiteDataReader object which contains data from the table
+        /// </summary>
+        /// <param name="tableName">Name of the table to look up</param>
+        /// <returns>SQLiteDataObject containing data instances</returns>
         public SQLiteDataReader ViewTable(string tableName)
         {
             string cmd = $"SELECT * FROM {tableName} order by id";
